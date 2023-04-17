d669cf8cca8d4dd0401e43e6807c954c192216e5
Andrea Contreras <andrea_ctr3007@hotmail.com>
2023-04-17 14:26:33 -0600

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            var mensaje = ConfigurationManager.AppSettings["mensaje"];
+            Console.WriteLine(mensaje);
+
+           
+            var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+            //using (SqlConnection sql = new SqlConnection(connectionString))
+            //{
+            //    sql.Open();
+            //}
+            //Console.Read();
+        }
+    }
+}
